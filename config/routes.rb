3a3c1032a9997391116028efcd17031451566eca
jcjohnny <jc2johnny@gmail.com>
2016-01-04 20:09:14 -0500

    added index

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ NautsRails::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root :to => 'articles#index'
+  root :to => 'pages#index'
   ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
